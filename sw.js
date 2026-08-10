245dec65220fa964d01cb33dab923d0816da4cc9
joeldl447-gif <joeldl447@gmail.com>
2026-08-10 10:14:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,26 @@
-const CACHE = "mis-finanzas-v3";
+const CACHE="mis-finanzas-v4";
+const A=["./","./index.html","./manifest.json","./icon.svg","./sw.js"];
 
-const ARCHIVOS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icono.svg"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ARCHIVOS))
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(A))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
+self.addEventListener("activate",e=>{
+  e.waitUntil(
+    caches.keys().then(k=>
       Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
+        k.filter(x=>x!==CACHE).map(x=>caches.delete(x))
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch",e=>
+  e.respondWith(
+    caches.match(e.request).then(c=>c||fetch(e.request))
+  )
+);
